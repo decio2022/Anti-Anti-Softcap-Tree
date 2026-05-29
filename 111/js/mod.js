ff9f04c39b4154058235609acfe7213735fed156
decio2022 <113923924+decio2022@users.noreply.github.com>
2026-05-29 15:16:29 -0300

    Added hardcap back bc the game inflates

--- 111/js/mod.js
+++ 111/js/mod.js
@@ -102,7 +102,7 @@ if(inChallenge("A",32)) gain=gain.log10()
 if(inChallenge("A",41)) gain=gain.tetrate(0.1)
 if(inChallenge("D",21)) gain=gain.slog()
 if(inChallenge("D",22)) gain=n(0)
-return gain.min("10^^10")
+return gain
 }
 function getPointGen() {
 	if(!canGenPoints())
